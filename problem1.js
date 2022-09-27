df05e363f1488e68a65b49f26dc497f1da0f11f6
VASSUNARESH <nareshkumar050396@gmail.com>
2022-09-27 20:35:15 +0530

    clear

---
+++ problem1.js
@@ -0,0 +1,17 @@
+let factors =0 ;
+let num = 15;
+for(let i=1;i<=num;i++){
+ if(num%i=0){
+ factors++;
+ } 
+}
+ if(factors==1) 
+  {
+  console.log("yes");
+  } 
+  else
+ { 
+ console.log("No"); 
+ }  
+
+
---
+++ problem2.js
@@ -0,0 +1,16 @@
+function checkPalindrome(N, str) {
+    let bag="";
+  for(i=str.length-1;i>=0;i--)
+  {  
+  bag=bag+str[i];
+  }
+  
+  if(str==bag)
+  {
+  console.log("Yes");
+  }
+  else
+  {
+  console.log("No");
+  }
+}
